cce7489a3ac6edbf82e927be4f342671d3ad28c8
Emery Berger <emery.berger@gmail.com>
2024-09-15 20:50:16 -0400

    Fixes errors caused when initializing with dict arg, among others.

--- randomdict.py
+++ randomdict.py
@@ -1,4 +1,3 @@
-
 # From https://stackoverflow.com/a/70870131
 
 try:
@@ -11,51 +10,65 @@ import random
 __version__ = '0.2.1'
 
 class RandomDict(MutableMapping):
-    def __init__(self, default_factory=None, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.default_factory = default_factory
-        self.keys = {}
-        self.values = []
+    def __init__(self, *args, **kwargs):
+        self._keys = {}
+        self._values = []
         self.last_index = -1
+        # Populate the RandomDict with any items passed in *args or **kwargs
+        self.update(*args, **kwargs)
+
+    def copy(self):
+        """ Return a shallow copy of the RandomDict """
+        new_rd = RandomDict()
+        new_rd._keys = self._keys.copy()          
+        new_rd._values = self._values[:]         
+        new_rd.last_index = self.last_index
+        return new_rd
+
+    @classmethod
+    def fromkeys(cls, keys, value=None):
+        """Create a RandomDict from an iterable of keys, all mapped to the same value."""
+        rd = cls()
+        for key in keys:
+            rd[key] = value
+        return rd
 
     def __setitem__(self, key, val):
-        i = self.keys.get(key, -1)
+        i = self._keys.get(key, -1)                
         if i > -1:
-            self.values[i] = (key, val)
+            self._values[i] = (key, val)          
         else:
             self.last_index += 1
             i = self.last_index
-            self.values.append((key, val))
-            self.keys[key] = i
+            self._values.append((key, val))       
+            self._keys[key] = i                   
     
     def __delitem__(self, key):
         # index of item to delete is i
-        i = self.keys[key]
+        i = self._keys[key]                        
         # last item in values array is
-        move_key, move_val = self.values.pop()
+        move_key, move_val = self._values.pop()    
 
         if i != self.last_index:
             # we move the last item into its location
-            self.values[i] = (move_key, move_val)
-            self.keys[move_key] = i
+            self._values[i] = (move_key, move_val)  
+            self._keys[move_key] = i                
         # else it was the last item and we just throw
         # it away
 
         # shorten array of values
         self.last_index -= 1
         # remove deleted key
-        del self.keys[key]
+        del self._keys[key]                        
     
     def __getitem__(self, key):
-        if key not in self.keys:
-            if self.default_factory is None:
-                raise KeyError(key)
-            self[key] = self.default_factory()
-        i = self.keys[key]
-        return self.values[i][1]
+        if key not in self._keys:                   
+            raise KeyError(key)
+        i = self._keys[key]                         
+        return self._values[i][1]                   
 
     def __iter__(self):
-        return iter(self.keys)
+        return iter(self._keys)                     
 
     def __len__(self):
         return self.last_index + 1
@@ -66,7 +79,7 @@ class RandomDict(MutableMapping):
             raise KeyError("RandomDict is empty")
         
         i = random.randint(0, self.last_index)
-        return self.values[i][0]
+        return self._values[i][0]                   
 
     def random_value(self):
         """ Return a random value from this dictionary in O(1) time """
@@ -76,6 +89,7 @@ class RandomDict(MutableMapping):
         """ Return a random key-value pair from this dictionary in O(1) time """
         k = self.random_key()
         return k, self[k]
+    
 
 def replace_dicts():
     # Replace dict with RandomDict
